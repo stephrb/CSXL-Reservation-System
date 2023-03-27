045a2fc4ac0004ec6d1b0a0d7cb02c577f06dab0
elliottsikes <esikes@email.unc.edu>
2023-03-27 16:04:15 -0400

    fixed improper naming conventions in backend/api/reservation.py

--- backend/api/reservation.py
+++ backend/api/reservation.py
@@ -6,5 +6,5 @@ from .authentication import registered_user
 api = APIRouter(prefix="/api/reservation")
 
 @api.get("", response_model=list[Reservation], tags=['Reservation'])
-def list_reservations(subject: User = Depends(registered_user), reg_svc: ReservationService = Depends()):
-    return reg_svc.list(subject)
+def list_reservations(subject: User = Depends(registered_user), res_svc: ReservationService = Depends()):
+    return res_svc.list(subject)
