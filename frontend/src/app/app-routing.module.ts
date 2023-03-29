08a3c80e2a757b5d2f159e9d56afa7ebd6c217bc
Brenna Mehl <77752369+brennamehl@users.noreply.github.com>
2023-03-29 14:11:46 -0400

    14-Angular-Setup (#22)
    * Add Reservation component and routing
    
    * Add Registration Service and tabs for Reservation page
    
    ---------
    
    Co-authored-by: Cam Burhans <77802045+camburhans@users.noreply.github.com>

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AppTitleStrategy } from './app-title.strategy';
 import { GateComponent } from './gate/gate.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileEditorComponent } from './profile/profile-editor/profile-editor.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 
 const routes: Routes = [
   HomeComponent.Route,
   ProfileEditorComponent.Route,
   GateComponent.Route,
+  ReservationComponent.Route,
   { path: 'admin', title: 'Admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
 ];
 
