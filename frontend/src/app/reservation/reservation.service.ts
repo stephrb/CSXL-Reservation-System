b468c03bc10485f56a6c295249f867d7511fd37b
brennamehl <brennam@email.unc.edu>
2023-03-30 10:24:32 -0400

    Fix getUserReservations to handle paginated reservations

--- frontend/src/app/reservation/reservation.service.ts
+++ frontend/src/app/reservation/reservation.service.ts
@@ -1,21 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
+import { Paginated } from '../pagination';
 
 export interface Reservation {
   id: number
   start_time: Date
   end_time:  Date
-  reservable_id: number
-  reservable: Reservable
-  user_id: number
-}
-
-export interface Reservable {
-  id: number
-  name: string
-  type: string
-  description: string
 }
 
 @Injectable({
@@ -25,14 +16,10 @@ export class ReservationService {
   constructor(private http: HttpClient) { }
 
   getUserReservations(): Observable<Reservation[]> {
-    return this.http.get<Reservation[]>("/api/reservation").pipe(map((reservations: Reservation[]) => reservations.map(reservation => {
+    return this.http.get<Paginated<Reservation>>("/api/reservation").pipe(map((paginatedReservations: Paginated<Reservation>) => paginatedReservations.items.map(reservation => {
       return {  id: reservation.id,
                 start_time: new Date(reservation.start_time),
-                end_time:  new Date(reservation.end_time), 
-                reservable_id: reservation.reservable_id, 
-                reservable: {id: reservation.reservable.id, name: reservation.reservable.name, type: reservation.reservable.type, 
-                  description: reservation.reservable.description}, 
-                user_id: reservation.user_id}
+                end_time:  new Date(reservation.end_time)}
     })
     ))
   }
