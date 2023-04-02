23650df03e92e3bcd668ed0364ec90d3238d9110
Stephen Buck <stephrb@ad.unc.edu>
2023-04-02 13:38:54 -0400

    Changed dev data so demo works properly

--- backend/script/dev_data/reservations.py
+++ backend/script/dev_data/reservations.py
@@ -3,12 +3,12 @@ from . import users, reservables
 from datetime import datetime
 
 res_1 =  Reservation(id=1,
-                    start_time=datetime(2023, 3, 22, 12, 0, 0, 0),
-                    end_time= datetime(2023, 3, 26, 15, 0, 0, 0))
+                    start_time=datetime(2023, 3, 25, 12, 0, 0, 0),
+                    end_time= datetime(2023, 3, 25, 15, 0, 0, 0))
 
 res_2 =  Reservation(id=2, 
-                     start_time=datetime(2023, 4, 3, 12, 0, 0, 0), 
-                     end_time=datetime(2023, 4, 3, 14, 30, 0, 0)
+                     start_time=datetime(2023, 4, 12, 12, 0, 0, 0), 
+                     end_time=datetime(2023, 4, 12, 14, 30, 0, 0)
                      )
 
 res_3 = Reservation(id=3, 
@@ -16,8 +16,8 @@ res_3 = Reservation(id=3,
                      end_time=datetime(2023, 3, 26, 16, 0, 0, 0)
                      )
 res_4 = Reservation(id=4, 
-                     start_time=datetime(2023, 4, 2, 9, 0, 0, 0), 
-                     end_time=datetime(2023, 4, 2, 12, 0, 0, 0)
+                     start_time=datetime(2023, 4, 13, 9, 0, 0, 0), 
+                     end_time=datetime(2023, 4, 13, 12, 0, 0, 0)
                      )
 
 triplets = [
