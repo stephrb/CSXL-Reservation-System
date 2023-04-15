b265b737b052597906bf19224a4d9ccf85fc01c5
elliottsikes <esikes@email.unc.edu>
2023-04-15 13:55:26 -0400

    Add all relevant docstrings to functions and methods in backend

--- backend/services/reservation.py
+++ backend/services/reservation.py
@@ -20,6 +20,7 @@ class ReservationService:
 
     
     def list_user_reservations_private(self, user: User, pagination_params: PaginationParams, op: operator) -> Paginated[Reservation] | None:
+        """Lists reservations associated with the given User"""
 
         statement = select(ReservationEntity).where(ReservationEntity.user_id == user.id).where(op(ReservationEntity.start_time, datetime.now()))
         length_statement = select(func.count()).select_from(ReservationEntity).where(ReservationEntity.user_id == user.id).where(op(ReservationEntity.start_time, datetime.now()))
@@ -40,29 +41,34 @@ class ReservationService:
     
 
     def list_user_reservations(self, user: User, pagination_params: PaginationParams, upcoming: bool) -> Paginated[Reservation] | None:
+        """Gets a list of reservations for a user based on if they are upcoming or past reservations"""
         if upcoming:
             return self.list_user_reservations_private(user, pagination_params, ge)
         else:
             return self.list_user_reservations_private(user, pagination_params, lt)
         
     def get_reservable(self, reservation_id: int) -> Reservable | None:
+        """Gets a reservable for a specific reservation"""
         statement = select(ReservableEntity).join(ReservationEntity).where(ReservationEntity.id==reservation_id)
         entity = self._session.scalar(statement)
         if entity:
             return entity.to_model()
     
     def get_reservation(self, reservation_id: int) -> Reservation | None:
+        """Gets a reservation given the reservation id number"""
         statement = select(ReservationEntity).where(ReservationEntity.id==reservation_id)
         entity = self._session.scalar(statement)
         if entity:
             return entity.to_model()
 
     def delete_reservation(self, reservation_id: int) -> None:
+        """Deletes a reservation given a reservation id number"""
         statement = delete(ReservationEntity).where(ReservationEntity.id==reservation_id)
         self._session.execute(statement)
         self._session.commit()
 
     def get_reservations_by_reservable(self, reservable_id: int, date: datetime) -> list[Reservation] | None:
+        """Gets a list of reservations given a reservable id number"""
         statement = select(ReservationEntity).where(ReservationEntity.reservable_id == reservable_id)\
         .filter(ReservationEntity.start_time >= date, ReservationEntity.start_time < date + timedelta(days=1))
         entities = self._session.scalars(statement)
